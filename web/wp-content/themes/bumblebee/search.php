ca1d91114688352cce0c53343bfddb64d936210f
Chris Hurst <chris@iamchrishurst.com>
2019-02-11 12:28:18 -0500

    DRDT-56: Updated search results page

--- web/wp-content/themes/bumblebee/search.php
+++ web/wp-content/themes/bumblebee/search.php
@@ -16,12 +16,13 @@ get_header();
 		<main id="main" class="site-main">
 		<?php if ( have_posts() ) : ?>
 			<header class="page-header">
-				<h1 class="page-title">
+				<h2 class="page-title">
 				<?php
 					/* translators: %s: search query. */
 					printf( esc_html__( 'Search Results for: %s', 'bumblebee' ), '<span>' . get_search_query() . '</span>' );
 				?>
-				</h1>
+				</h2>
+				<hr>
 			</header><!-- .page-header -->
 
 			<?php
--- web/wp-content/themes/bumblebee/template-parts/content-search.php
+++ web/wp-content/themes/bumblebee/template-parts/content-search.php
@@ -10,23 +10,26 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="post-image">
-		<?php bumblebee_post_thumbnail( 'medium' ); ?>
+	<div class="content-image">
+		<?php bumblebee_post_thumbnail( 'large' ); ?>
 	</div>
-	<header class="entry-header">
-	<?php
-	if ( 'post' === get_post_type() ) {
-		$categories_list = get_the_category_list( esc_html__( ', ', 'bumblebee' ) );
-		if ( $categories_list ) {
-			/* translators: 1: list of categories. */
-			printf( '<span class="cat-links">' . esc_html__( '%1$s', 'bumblebee' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput
+	<div class="content-container">
+		<header class="entry-header">
+		<?php
+		if ( 'post' === get_post_type() ) {
+			$categories_list = get_the_category_list( esc_html__( ', ', 'bumblebee' ) );
+			if ( $categories_list ) {
+				/* translators: 1: list of categories. */
+				printf( '<span class="cat-links">' . esc_html__( '%1$s', 'bumblebee' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput
+			}
 		}
-	}
-		the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' );
-	?>
-	</header><!-- .entry-header -->
+			the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' );
+		?>
+		</header><!-- .entry-header -->
 
-	<div class="entry-summary">
-	<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+		<div class="entry-summary">
+		<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+	</div><!-- .content-container -->
+	
 </article><!-- #post-<?php the_ID(); ?> -->
