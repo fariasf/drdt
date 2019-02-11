3b3d40e7ae64018dc1085c8d8f5b05a215e9f389
Chris Hurst <chris@iamchrishurst.com>
2019-02-11 13:45:21 -0500

    DRDT-56: Fix translatable content

--- web/wp-content/themes/bumblebee/template-parts/content-search.php
+++ web/wp-content/themes/bumblebee/template-parts/content-search.php
@@ -18,11 +18,12 @@
 		<?php
 		if ( 'post' === get_post_type() ) {
 			$categories_list = get_the_category_list( esc_html__( ', ', 'bumblebee' ) );
-			if ( $categories_list ) { ?>
-				<span class="cat-links">
-				<?php printf( esc_html__( '%1$s', 'bumblebee' ), $categories_list ); ?>
-				</span>
-			<?php }
+			if ( $categories_list ) {
+				echo '<span class="cat-links">';
+				/* translators: 1: list of categories. */
+				printf( esc_html__( '%1$s', 'bumblebee' ), $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput
+				echo '</span>';
+			}
 		}
 			the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' );
 		?>
