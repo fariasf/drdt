cbd18fca27ec9f722dd5bb983518a89f741d013f
Facundo Farias <contacto@facundofarias.com.ar>
2019-02-14 14:34:04 -0300

    Add instant.page script for preloading

--- web/wp-content/themes/bumblebee/footer.php
+++ web/wp-content/themes/bumblebee/footer.php
@@ -114,5 +114,6 @@ get_hamburger_menu_markup();
 echo '</div>';
 
 ?>
+<script src="//instant.page/1.1.0" type="module" integrity="sha384-EwBObn5QAxP8f09iemwAJljc+sU+eUXeL9vSBw1eNmVarwhKk2F9vBEpaN9rsrtp"></script>
 </body>
 </html>
