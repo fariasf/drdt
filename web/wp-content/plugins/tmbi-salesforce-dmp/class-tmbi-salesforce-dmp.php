00cf381a5a50ebce1b9dc74b1146063470556889
akhan3051 <ayub.khan@ness.com>
2019-02-21 02:01:43 +0530

    DRDT-106: Fixed validation errors and code refactoring

--- web/wp-content/plugins/tmbi-salesforce-dmp/class-tmbi-salesforce-dmp.php
+++ web/wp-content/plugins/tmbi-salesforce-dmp/class-tmbi-salesforce-dmp.php
@@ -1,6 +1,6 @@
 <?php
 /**
-Ad Stack
+TMBI Salesforce DMP
 
 @package TMBI Salesforce DMP
 Plugin Name: TMBI Salesforce DMP
@@ -8,32 +8,55 @@ Version: 1.0.0
 Description: This plugin adds Salesforce DMP tags to the header and uses GPT to call Krux segments <a href='https://readersdigest.atlassian.net/browse/PLT-459' target='_blank'>Read more at PLT-459 ...</a>
 Author: DRDT Team
 Text Domain: tmbi-salesforce-dmp
-*/
-//DEBUG::enable_error_reporting();
+ */
+
+/**
+ * Including file for author details.
+ *
+ * @file
+ */
+require_once 'inc/author-detail.php';
 
 /**
  *  Class TMBI_Salesforce_DMP.
  */
 class TMBI_Salesforce_DMP {
-	const VERSION                   = '1.0.0';
-	const DEPENDS                   = null;
-	const PRIORITY                  = 1;
-	const SCRIPT_VAR                = 'krux_data';
-	const SCRIPT_SLUG               = 'krux-click-tracking';
-	const SCRIPT_URL                = 'js/krux-click-traking.js';
-	const CONTROL_TAG_START         = '<!-- BEGIN Salesforce DMP ControlTag for "tmbi.com" -->';
-	const CONTROL_TAG_END           = '<!-- END Salesforce DMP ControlTag -->';
-	const CONTROL_SCRIPT_TAG        = "<script class='kxct' data-id='s9xpab5u5' data-timing='async' data-version='3.0' type='text/javascript'>%s</script>";
-	const INTERCHANGE_SCRIPT_TAG    = "<script class='kxint' data-namespace='trustedmediabrandsinc' type='text/javascript'>%s</script>";
+	const VERSION                = '1.0.0';
+	const DEPENDS                = null;
+	const PRIORITY               = 1;
+	const SCRIPT_VAR             = 'krux_data';
+	const SCRIPT_SLUG            = 'krux-click-tracking';
+	const SCRIPT_URL             = 'js/krux-click-traking.js';
+	const CONTROL_TAG_START      = '<!-- BEGIN Salesforce DMP ControlTag for "tmbi.com" -->';
+	const CONTROL_TAG_END        = '<!-- END Salesforce DMP ControlTag -->';
+	const CONTROL_SCRIPT_TAG     = "<script class='kxct' data-id='s9xpab5u5' data-timing='async' data-version='3.0' type='text/javascript'>%s</script>";
+	const INTERCHANGE_SCRIPT_TAG = "<script class='kxint' data-namespace='trustedmediabrandsinc' type='text/javascript'>%s</script>";
 
+	/**
+	 * Variable to exclude the taxonomies
+	 *
+	 * @var array
+	 */
 	public static $excluded_taxonomies = array( 'yst_prominent_words', 'post_tag', 'category', 'exclude_feed', 'post_format', 'author' );
-	public static $krux_data_layer     = array();
 
+	/**
+	 * Variable to build the krux data for localization
+	 *
+	 * @var array
+	 */
+	public static $krux_data_layer = array();
+
+	/**
+	 * Initiate the methods
+	 */
 	public static function init() {
 		add_action( 'wp_head', array( __CLASS__, 'render_salesforce_dmp_tags' ), self::PRIORITY );
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'load_krux_click_tracking' ) );
 	}
 
+	/**
+	 * Localize the Krux targeting data
+	 */
 	public static function load_krux_click_tracking() {
 		wp_register_script(
 			self::SCRIPT_SLUG,
@@ -51,6 +74,9 @@ class TMBI_Salesforce_DMP {
 		}
 	}
 
+	/**
+	 * Add the salesforce tags
+	 */
 	public static function render_salesforce_dmp_tags() {
 
 		$control_tag_script = PHP_EOL . "window.Krux||((Krux=function(){Krux.q.push(arguments)}).q=[]);
@@ -61,7 +87,6 @@ class TMBI_Salesforce_DMP {
   }());" . PHP_EOL;
 
 		$contol_tag = sprintf( PHP_EOL . PHP_EOL . self::CONTROL_TAG_START . PHP_EOL . self::CONTROL_SCRIPT_TAG . PHP_EOL . self::CONTROL_TAG_END . PHP_EOL, $control_tag_script );
-
 
 		$interchage_tag_script = PHP_EOL . "window.Krux||((Krux=function(){Krux.q.push(arguments);}).q=[]);
 (function(){
@@ -86,10 +111,25 @@ class TMBI_Salesforce_DMP {
   Krux.segments = retrieve('segs') ? retrieve('segs').split(',') : [];
 })();" . PHP_EOL;
 
+		$allowed_html   = [
+			'script' => [
+				'class'          => [],
+				'data-id'        => [],
+				'data-timing'    => [],
+				'data-version'   => [],
+				'type'           => [],
+				'data-namespace' => [],
+			],
+		];
 		$interchage_tag = sprintf( PHP_EOL . self::INTERCHANGE_SCRIPT_TAG, $interchage_tag_script );
-		echo $contol_tag . $interchage_tag ;
+		echo wp_kses( $contol_tag . $interchage_tag, $allowed_html );
 	}
 
+	/**
+	 * Function to build the krux data layer
+	 *
+	 * @return array
+	 */
 	public static function krux_build_datalayer() {
 
 		if ( ! is_single() ) {
@@ -106,12 +146,12 @@ class TMBI_Salesforce_DMP {
 			$keywords   = self::get_post_tags();
 		}
 
-		$contentId = get_the_ID();
+		$content_id = get_the_ID();
 		if ( is_array( $categories ) ) {
-			if ( array_key_exists( 'parent_categories', $categories ) && ( class_exists( 'TMBI_Ad_Stack' ) ) ) {
+			if ( array_key_exists( 'parent_categories', $categories ) && ( class_exists( 'Ad_Stack' ) ) ) {
 				$top_level_categories = self::format_data( $categories['parent_categories'] );
 			}
-			if ( array_key_exists( 'sub_categories', $categories ) && ( class_exists( 'TMBI_Ad_Stack' ) ) ) {
+			if ( array_key_exists( 'sub_categories', $categories ) && ( class_exists( 'Ad_Stack' ) ) ) {
 				$sub_categories = self::format_data( $categories['sub_categories'] );
 			}
 		}
@@ -122,42 +162,52 @@ class TMBI_Salesforce_DMP {
 			'keyword'  => $keywords,
 		);
 
-		self::$krux_data_layer['page']['contentid'] = $contentId;
-		self::$krux_data_layer['page']['author']    = self::get_post_authors();
-		self::$krux_data_layer['page']['gs_categories'] = json_decode( get_post_meta( $contentId, 'gs_channels' , true ) );
+		self::$krux_data_layer['page']['contentid']     = $content_id;
+		self::$krux_data_layer['page']['author']        = self::get_post_authors();
+		self::$krux_data_layer['page']['gs_categories'] = json_decode( get_post_meta( $content_id, 'gs_channels', true ) );
 
 		$tax_array = self::get_post_taxonomy();
 		if ( $tax_array && ( class_exists( 'Ad_Stack' ) ) ) {
 			foreach ( $tax_array as $tax => $value ) {
-				self::$krux_data_layer['page'][$tax] = self::format_data( $value );
+				self::$krux_data_layer['page'][ $tax ] = self::format_data( $value );
 			}
 		}
 		return self::$krux_data_layer;
 	}
 
+	/**
+	 * Get the taxonomies of the posts
+	 *
+	 * @return array|string
+	 */
 	public static function get_post_taxonomy() {
 		if ( ! is_single() ) {
 			return '';
 		}
 		$taxonomies_targeting = array();
-		$taxonomies = get_object_taxonomies( get_post() );
-		$relevant_taxonomies = array_filter( $taxonomies, function( $taxonomy ) {
-			return ! in_array( $taxonomy, self::$excluded_taxonomies );
-		});
-		$relevant_taxonomies = array_values( $relevant_taxonomies );
-		$terms = wp_get_post_terms( get_the_ID(), $relevant_taxonomies );
+		$taxonomies           = get_object_taxonomies( get_post() );
+		$relevant_taxonomies  = array_filter(
+			$taxonomies,
+			function( $taxonomy ) {
+				return ! in_array( $taxonomy, self::$excluded_taxonomies, true );
+			}
+		);
+		$relevant_taxonomies  = array_values( $relevant_taxonomies );
+		$terms                = wp_get_post_terms( get_the_ID(), $relevant_taxonomies );
 		foreach ( $terms as $term ) {
-			$taxonomies_targeting[$term->taxonomy][] = strtolower( str_replace( ' ', '_', $term->name ) );
+			$taxonomies_targeting[ $term->taxonomy ][] = strtolower( str_replace( ' ', '_', $term->name ) );
 		}
 		return $taxonomies_targeting;
 	}
 
+	/**
+	 * Get the authors of the post
+	 *
+	 * @return array|string
+	 */
 	public static function get_post_authors() {
 		$author_list_format = '';
-		$author_list        = '';
-		if ( class_exists( 'TMBI_Author_Detail' ) ) {
-			$author_list = TMBI_Author_Detail::get_the_authors();
-		}
+		$author_list        = get_the_authors();
 		if ( $author_list ) {
 			$author_list_format = array_column( $author_list, 'author_name' );
 		}
