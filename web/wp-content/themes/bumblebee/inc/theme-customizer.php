ba5fb247554584388347f37f374290fa8b8b8fc4
Chris Hurst <chris@iamchrishurst.com>
2019-03-07 13:26:06 -0500

    DRDT-112: Adding header code back

--- web/wp-content/themes/bumblebee/inc/theme-customizer.php
+++ web/wp-content/themes/bumblebee/inc/theme-customizer.php
@@ -811,6 +811,14 @@ function bumblebee_add_customizer_styles() {
 			border-bottom: 2px dotted <?php echo esc_html( $nav_text_color ); ?>;
 		}
 
+		.header .main-navigation.sticky #search-form-wrapper.visible,
+		.header .main-navigation.sticky #search-form-wrapper.visible .close-btn {
+			background-color: <?php echo esc_html( $nav_bg_color ); ?>;
+		}
+		.header .main-navigation.sticky #search-form-wrapper.visible .search-button {
+			background-color: <?php echo esc_html( $button_bg_color ); ?>;
+		}
+
 		.newsletter-sign-below-header.hide-on-mobile .nl-signup-link h4,
 		.newsletter-sign-below-header.hide-on-mobile .nl-signup-link h4 .nl-right-arrow {
 			color: <?php echo esc_html( $banner_text_color ); ?>;
