69fb19c3368db316ecf694a754358c81431f29e0
Chris Hurst <chris@iamchrishurst.com>
2019-03-13 09:31:29 -0400

    DRDT-99: Added customizer field for FB:ID

--- web/wp-content/themes/bumblebee/inc/theme-customizer.php
+++ web/wp-content/themes/bumblebee/inc/theme-customizer.php
@@ -757,6 +757,34 @@ function bumblebee_custom_customize_register( $wp_customize ) {
 			),
 		)
 	);
+
+	$wp_customize->add_section(
+		'bumblebee_facebook_id',
+		array(
+			'title'       => __( 'Facebook Meta Tag', 'bumblebee' ),
+			'description' => __( 'Add the FB ID' ),
+			'priority'    => 45,
+		)
+	);
+
+	$wp_customize->add_setting(
+		'bumblebee_facebook_id_string',
+		array(
+			'capability'        => 'edit_theme_options',
+			'sanitize_callback' => 'sanitize_text_field',
+		)
+	);
+
+	$wp_customize->add_control(
+		'bumblebee_facebook_id_string',
+		array(
+			'type'        => 'text',
+			'section'     => 'bumblebee_facebook_id',
+			'label'       => __( 'Facebook ID' ),
+			'description' => __( 'Enter FB ID here, i.e: 1234567890' ),
+			'settings'    => 'bumblebee_facebook_id_string',
+		)
+	);
 }
 add_action( 'customize_register', 'bumblebee_custom_customize_register' );
 
