8bda405d21505a984d2ca4546af7352eca0ddfce
samuel kishore kumar <samuelkishorekumar26@gmail.com>
2019-04-02 15:58:03 +0530

    DRDT-77: Removed the social share

--- web/wp-content/themes/bumblebee/template-parts/social-share.php
+++ web/wp-content/themes/bumblebee/template-parts/social-share.php
@@ -10,9 +10,4 @@
  */
 
 do_action( 'print_custom_social_share' );
-?>
-<!-- <ul class="pure-menu-list social-menu">
-	<li class="social-share-item"><a href="#" class="pure-menu-link"><img class="social-icons" src=" <?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/envelope-regular.svg" /></a></li>
-	<li class="social-share-item"><a href="#" class="pure-menu-link"><img class="social-icons" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/facebook-f-brands.svg" /></a></li>
-	<li class="social-share-item"><a href="#" class="pure-menu-link"><img class="social-icons" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/pinterest-p-brands.svg" /></a></li>
-</ul> -->
+
