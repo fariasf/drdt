600b9c036c791295ae94efcd2679c1701d0e3933
samuel kishore kumar <samuelkishorekumar26@gmail.com>
2019-04-02 15:52:40 +0530

    DRDT-77: Fixed the class loading and social share

--- web/wp-content/plugins/tmbi-custom-social-share/class-tmbi-custom-social-share.php
+++ web/wp-content/plugins/tmbi-custom-social-share/class-tmbi-custom-social-share.php
@@ -25,6 +25,13 @@ require 'inc/load-krux-tracking.php';
 require 'inc/register-acf-long-pin-image-field.php';
 
 /**
+ * Including file for register wpseo opengraph twitter title and desc.
+ *
+ * @file
+ */
+require 'inc/class-wpseo-opengraph-twitter-title.php';
+
+/**
  * Class TMBI_Custom_Social_Share
  */
 class TMBI_Custom_Social_Share extends WPSEO_Opengraph_Twitter_Title {
@@ -65,38 +72,52 @@ class TMBI_Custom_Social_Share extends WPSEO_Opengraph_Twitter_Title {
 				$page_image = get_the_post_thumbnail_url( get_the_ID(), 'full' );
 			}
 			$position_prop = 'social share-left sticky nav';
-
+			$allowed_tags  = array(
+				'a'   => array(
+					'class'                  => array(),
+					'data-analytics-metrics' => array(),
+					'id'                     => array(),
+					'onClick'                => array(),
+					'href'                   => array(),
+					'target'                 => array(),
+				),
+				'img' => array(
+					'class' => array(),
+					'src'   => array(),
+					'alt'   => array(),
+				),
+			);
 			print( '<ul class="pure-menu-list social-menu">' );
 			foreach ( self::$default_share_icons as $social_icons ) {
 				print( '<li class="social-share-item">' );
 				switch ( strtoupper( trim( $social_icons ) ) ) {
 					case 'FACEBOOK':
-						echo wp_kses_post( '<a class="pure-menu-link" data-analytics-metrics=' . self::tmbi_adobe_analytics( 'facebook', $position_prop ) . ' id="fb-share" onClick="window.open(\'https://www.facebook.com/sharer/sharer.php?u=' . rawurlencode( $page_url ) . '&t' . rawurlencode( $page_title ) . '=&v=3\',\'sharer\',\'toolbar=0,status=0,width=548,height=325\');" href="javascript: void(0)"><img class="social-icons" src="' . plugin_dir_url( __FILE__ ) . '/images/facebook-letter-logo.svg" alt="facebook" /></a>' );
+						echo wp_kses( '<a class="pure-menu-link" data-analytics-metrics=' . self::tmbi_adobe_analytics( 'facebook', $position_prop ) . ' id="fb-share" onClick="window.open(\'https://www.facebook.com/sharer/sharer.php?u=' . rawurlencode( $page_url ) . '&t' . rawurlencode( $page_title ) . '=&v=3\',\'sharer\',\'toolbar=0,status=0,width=548,height=325\');" href="javascript: void(0)"><img class="social-icons" src="' . plugin_dir_url( __FILE__ ) . '/images/facebook-letter-logo.svg" alt="facebook" /></a>', $allowed_tags );
 
 						break;
 
 					case 'TWITTER':
-						echo wp_kses_post( '<a class="pure-menu-link" data-analytics-metrics=' . self::tmbi_adobe_analytics( 'twitter', $position_prop ) . ' id="twitter-share" onClick="window.open(\'https://twitter.com/share?url=' . rawurlencode( $page_url ) . '&amp;text=' . rawurlencode( $page_title ) . '&amp;hashtags=\',\'sharer\',\'toolbar=0,status=0,width=548,height=325\');" href="javascript: void(0)"><img class="social-icons" src="' . plugin_dir_url( __FILE__ ) . './images/twitter-logo.svg" alt="twitter" /></a>' );
+						echo wp_kses( '<a class="pure-menu-link" data-analytics-metrics=' . self::tmbi_adobe_analytics( 'twitter', $position_prop ) . ' id="twitter-share" onClick="window.open(\'https://twitter.com/share?url=' . rawurlencode( $page_url ) . '&amp;text=' . rawurlencode( $page_title ) . '&amp;hashtags=\',\'sharer\',\'toolbar=0,status=0,width=548,height=325\');" href="javascript: void(0)"><img class="social-icons" src="' . plugin_dir_url( __FILE__ ) . './images/twitter-logo.svg" alt="twitter" /></a>', $allowed_tags );
 
 						break;
 
 					case 'PINTEREST':
-						echo wp_kses_post( '<a class="pure-menu-link" data-analytics-metrics=' . self::tmbi_adobe_analytics( 'pinterest', $position_prop ) . ' id="pinterest-share" onClick="window.open(\'https://pinterest.com/pin/create/button/?url=' . rawurlencode( $page_url ) . '&media=' . rawurlencode( $page_image ) . '&description=' . rawurlencode( $page_title ) . ',\'sharer\',\'toolbar=0,status=0,width=548,height=325\');" href="javascript: void(0)"><img class="social-icons" src="' . plugin_dir_url( __FILE__ ) . './images/pinterest-social-visual-website-logotype.svg" alt="Pinterest" /></a>' );
+						echo wp_kses( '<a class="pure-menu-link" data-analytics-metrics=' . self::tmbi_adobe_analytics( 'pinterest', $position_prop ) . ' id="pinterest-share" onClick="window.open(\'https://pinterest.com/pin/create/button/?url=' . rawurlencode( $page_url ) . '&media=' . rawurlencode( $page_image ) . '&description=' . rawurlencode( $page_title ) . ',\'sharer\',\'toolbar=0,status=0,width=548,height=325\');" href="javascript: void(0)"><img class="social-icons" src="' . plugin_dir_url( __FILE__ ) . './images/pinterest-social-visual-website-logotype.svg" alt="Pinterest" /></a>', $allowed_tags );
 
 						break;
 
 					case 'EMAIL':
-						echo wp_kses_post( '<li class="social-share-item"><a class="pure-menu-link" data-analytics-metrics=' . self::tmbi_adobe_analytics( 'email', $position_prop ) . '  id="email_a_friend" onClick="window.open(\'' . self::tmbi_custom_email() . '\',\'_self\')" href="javascript: void(0)" > <img class="social-icons" src="' . plugin_dir_url( __FILE__ ) . './images/envelope.svg" alt="Email" /></a></li> ' );
+						echo wp_kses( '<a class="pure-menu-link" data-analytics-metrics=' . self::tmbi_adobe_analytics( 'email', $position_prop ) . '  id="email_a_friend" onClick="window.open(\'' . self::tmbi_custom_email() . '\',\'_self\')" href="javascript: void(0)" > <img class="social-icons" src="' . plugin_dir_url( __FILE__ ) . './images/envelope.svg" alt="Email" /></a>', $allowed_tags );
 
 						break;
 
 					case 'LINKEDIN':
-						echo wp_kses_post( '<a class="pure-menu-link" target="_blank" id="linkedin-share" onClick="window.open(\'https://www.linkedin.com/shareArticle?mini=true&url=' . $page_url . '&title=' . $page_title . '&summary=' . $page_desc . '&source=\',\'sharer\',\'toolbar=0,status=0,width=548,height=325\');" href="javascript: void(0)"> <img class="social-icons" src="' . plugin_dir_url( __FILE__ ) . './images/linkedin.svg" alt="Linkedin" /></a> ' );
+						echo wp_kses( '<a class="pure-menu-link" target="_blank" id="linkedin-share" onClick="window.open(\'https://www.linkedin.com/shareArticle?mini=true&url=' . $page_url . '&title=' . $page_title . '&summary=' . $page_desc . '&source=\',\'sharer\',\'toolbar=0,status=0,width=548,height=325\');" href="javascript: void(0)"> <img class="social-icons" src="' . plugin_dir_url( __FILE__ ) . './images/linkedin.svg" alt="Linkedin" /></a>', $allowed_tags );
 
 						break;
 
 					case 'REDDIT':
-						echo wp_kses_post( '<a class="pure-menu-link" target="_blank" id="reddit-share" onClick="window.open(\'http://reddit.com/submit?url=' . $page_url . '&amp;title=' . $page_title . '\',\'sharer\',\'toolbar=0,status=0,width=548,height=325\');" href="javascript: void(0)"> <img class="social-icons" src="' . plugin_dir_url( __FILE__ ) . './images/reddit.svg" alt="Reddit" /></a>' );
+						echo wp_kses( '<a class="pure-menu-link" target="_blank" id="reddit-share" onClick="window.open(\'http://reddit.com/submit?url=' . $page_url . '&amp;title=' . $page_title . '\',\'sharer\',\'toolbar=0,status=0,width=548,height=325\');" href="javascript: void(0)"> <img class="social-icons" src="' . plugin_dir_url( __FILE__ ) . './images/reddit.svg" alt="Reddit" /></a>', $allowed_tags );
 
 						break;
 
