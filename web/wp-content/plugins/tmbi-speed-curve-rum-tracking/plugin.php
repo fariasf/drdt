0a630ff1b936a304f179456059d079beaf4ca1ed
Prasanth Bendra <prasanth.bendra@ness.com>
2019-04-04 10:40:43 +0530

    DRDT-148 - Fix validatation issues

--- web/wp-content/plugins/tmbi-speed-curve-rum-tracking/plugin.php
+++ web/wp-content/plugins/tmbi-speed-curve-rum-tracking/plugin.php
@@ -11,14 +11,14 @@
 
 class TMBI_SPEED_CURVE_RUM_TRACKING {
 
-	const VERSION 				= '1.0.0';
-	const DEPENDS 				= 'jquery';
-	const IN_FOOTER 			= true;
-	const PRIORITY 				= 10;
-	const SCRIPT_NAME 			= 'speed-curve';
-	const SCRIPT_FILE 			= 'js/speed-curve.js';
-	const SC_SCRIPT_URL 		= 'https://cdn.speedcurve.com/js/lux.js?id=128112127';
-	const SC_SCRIPT_SLUG 		= 'speedcurve-base';
+	const VERSION               = '1.0.0';
+	const DEPENDS               = 'jquery';
+	const IN_FOOTER             = true;
+	const PRIORITY              = 10;
+	const SCRIPT_NAME           = 'speed-curve';
+	const SCRIPT_FILE           = 'js/speed-curve.js';
+	const SC_SCRIPT_URL         = 'https://cdn.speedcurve.com/js/lux.js?id=128112127';
+	const SC_SCRIPT_SLUG        = 'speedcurve-base';
 	const CUSTOM_SCRIPT_URL     = 'js/custom-script.js';
 	const CUSTOM_SCRIPT_SLUG    = 'custom-script';
 
@@ -28,17 +28,29 @@ class TMBI_SPEED_CURVE_RUM_TRACKING {
 
 	public function tmbi_speed_curve_script() {
 		wp_register_script(
-				self::SCRIPT_NAME, plugins_url( self::SCRIPT_FILE, __FILE__ ), array( self::DEPENDS ), self::VERSION, self::IN_FOOTER
+			self::SCRIPT_NAME,
+			plugins_url( self::SCRIPT_FILE, __FILE__ ),
+			array( self::DEPENDS ),
+			self::VERSION,
+			self::IN_FOOTER
 		);
 		wp_enqueue_script( self::SCRIPT_NAME );
 
 		wp_register_script(
-				self::SC_SCRIPT_SLUG, self::SC_SCRIPT_URL, [], self::VERSION, self::IN_FOOTER
+			self::SC_SCRIPT_SLUG,
+			self::SC_SCRIPT_URL,
+			[],
+			self::VERSION,
+			self::IN_FOOTER
 		);
 		wp_enqueue_script( self::SC_SCRIPT_SLUG );
 
 		wp_register_script(
-			self::CUSTOM_SCRIPT_SLUG, plugins_url( self::CUSTOM_SCRIPT_URL, __FILE__ ), array( 'jquery', self::SC_SCRIPT_SLUG, self::SCRIPT_NAME ), self::VERSION, self::IN_FOOTER
+			self::CUSTOM_SCRIPT_SLUG,
+			plugins_url( self::CUSTOM_SCRIPT_URL, __FILE__ ),
+			array( 'jquery', self::SC_SCRIPT_SLUG, self::SCRIPT_NAME ),
+			self::VERSION,
+			self::IN_FOOTER
 		);
 		wp_localize_script( self::CUSTOM_SCRIPT_SLUG, 'LUX_label', $this->get_LUX_label() );
 		wp_enqueue_script( self::CUSTOM_SCRIPT_SLUG );
@@ -61,7 +73,7 @@ class TMBI_SPEED_CURVE_RUM_TRACKING {
 			'page'          => 'Content Page',
 		);
 
-		$result = isset( $labels[$page_type] )? $labels[$page_type] : null;
+		$result = isset( $labels[ $page_type ] ) ? $labels[ $page_type ] : null;
 		return $result;
 	}
 
